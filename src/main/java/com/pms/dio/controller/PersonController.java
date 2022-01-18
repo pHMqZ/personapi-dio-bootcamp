ced1527352301192f1ae6c7ef7199fcf646c231b
Phillip Marques <phillipmarq@hotmail.com>
2022-01-17 23:22:03 -0300

    intial commit

---
+++ src/main/java/com/pms/dio/controller/PersonController.java
@@ -0,0 +1,10 @@
+package com.pms.dio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/people")
+public class PersonController {
+
+}
